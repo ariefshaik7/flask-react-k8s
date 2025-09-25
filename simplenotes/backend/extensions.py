0a3bcb9f02766a9266f0649ccf33c047d9958783
arief <ariefshaik001@gmail.com>
2025-09-25 12:14:52 +0530

    backend

--- simplenotes/backend/extensions.py
+++ simplenotes/backend/extensions.py
@@ -1,9 +1,12 @@
+# AFTER
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from prometheus_flask_exporter import PrometheusMetrics
+from flask_jwt_extended import JWTManager # <-- IMPORT THIS
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 cors = CORS()
 metrics = PrometheusMetrics.for_app_factory()
+jwt = JWTManager() # <-- ADD THIS LINE
